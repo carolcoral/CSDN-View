f90841c690e81dd1d7c89ded24b3f8b5bd911599
CarolLiu <carolcoral@users.noreply.github.com>
2018-01-24 14:37:49 +0800

    Update count.py

--- count.py
+++ count.py
@@ -1,27 +1,30 @@
-
-# -*- coding: utf-8 -* #设置编码格式
-
+# -*- coding: utf-8 -*
 import webbrowser as web    
 import time    
 import os    
 import random
 import requests
-
-count = random.randint(1,2)    
-j=0    
-while j<count:    
+#s = requests.seesion()  
+count = random.randint(1,2)   
+urllist=[
+'http://blog.csdn.net/carolcoral/article/details/79105562',
+'http://blog.csdn.net/carolcoral/article/details/79105562',
+'http://blog.csdn.net/carolcoral/article/details/79041873',
+'http://blog.csdn.net/carolcoral/article/details/79034589',
+'http://blog.csdn.net/carolcoral/article/details/79022987',
+'http://blog.csdn.net/carolcoral/article/details/79016608',
+'http://blog.csdn.net/carolcoral/article/details/79009155',
+'http://blog.csdn.net/carolcoral/article/details/78895236']
+for j in range(0,100000):#设置循环的总次数   
     i=0    
-    while i<=8 :  
-        s = requests.session()  #获取session 值
-        s.get('http://blog.csdn.net')  #获取指定网址的 coockie 值
-        web.open_new_tab('http://blog.csdn.net/carolcoral/article/details/79105562')  #网址替换这里  
-        s.cookies.clear() # 每次访问后清除 coockie
-        i=i+1    
-        time.sleep(3)  #这个时间根据自己电脑处理速度设置，单位是s  #每次刷新的间隔时间
+    while i<=1 :  #一次打开浏览器访问的循环次数
+        #s.get('http://blog.csdn.net')  
+        for url in urllist:
+            web.open(url)  #访问网址地址，语法 .open(url,new=0,Autorasise=True),设置 new 的值不同有不同的效果0、1、2  
+            i=i+1    
+            time.sleep(3)  #设置每次打开新页面的等待时间
     else:    
-        time.sleep(10) #这个时间根据自己电脑处理速度设置，单位是s 休眠间隔时间
-        os.system('taskkill /Applications/Firefox.app')  #火狐浏览器，其他的更换下就行，这里设置的是你电脑的默认打开浏览器
-        #print 'time webbrower closed'  
-          
-    j=j+1    
- 
+        time.sleep(10) #设置每次等待关闭浏览器的时间  
+        os.system('taskkill /F /IM firefox.app')  #你设置的默认使用浏览器，其他的更换下就行
+        #/F强制关闭进程  /T关闭的进程树及子树  /IM进程的映像名称
+        #print 'time webbrower closed'    
