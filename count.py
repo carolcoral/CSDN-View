a3024f8085ebc5c71c6ca9b46353bb32706be2a5
CarolLiu <carolcoral@users.noreply.github.com>
2018-01-25 11:24:52 +0800

    Update count.py

--- count.py
+++ count.py
@@ -5,19 +5,23 @@ import os
 import random
 import requests
 #s = requests.seesion()  
-count = random.randint(1,2)   
+#count = random.randint(1,2)   
 urllist=[
-'http://blog.csdn.net/carolcoral/article/details/79105562',
+'http://blog.csdn.net/carolcoral/article/details/78789454',
+'http://blog.csdn.net/carolcoral/article/details/77857264',
 'http://blog.csdn.net/carolcoral/article/details/79105562',
 'http://blog.csdn.net/carolcoral/article/details/79041873',
 'http://blog.csdn.net/carolcoral/article/details/79034589',
 'http://blog.csdn.net/carolcoral/article/details/79022987',
 'http://blog.csdn.net/carolcoral/article/details/79016608',
+'http://blog.csdn.net/carolcoral/article/details/79009252',
 'http://blog.csdn.net/carolcoral/article/details/79009155',
-'http://blog.csdn.net/carolcoral/article/details/78895236']
+'http://blog.csdn.net/carolcoral/article/details/79009058',
+'http://blog.csdn.net/carolcoral/article/details/79009033',
+'http://blog.csdn.net/carolcoral/article/details/78898494']
 for j in range(0,100000):#设置循环的总次数   
     i=0    
-    while i<=1 :  #一次打开浏览器访问的循环次数
+    while i<1 :  #一次打开浏览器访问的循环次数
         #s.get('http://blog.csdn.net')  
         for url in urllist:
             web.open(url)  #访问网址地址，语法 .open(url,new=0,Autorasise=True),设置 new 的值不同有不同的效果0、1、2  
